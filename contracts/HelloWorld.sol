df1d0e6baa0de011b07e671ee07ff94f8740130a
Tien Do <tiendq@gmail.com>
2022-01-28 22:09:46 +0700

    concat strings

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,10 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-  string public greeting = "hello";
+  string public greeting = 'hello';
+  uint public constant GREETING_COUNT = 0;
+  address public immutable deployer;
+
+  constructor() {
+    deployer = msg.sender;
+  }
 
   function sayHello() external view returns (string memory) {
-    return greeting;
+    return string(abi.encodePacked(greeting, ' from ', abi.encodePacked(deployer)));
   }
 
   function updateGreeting(string calldata _greeting) external {
