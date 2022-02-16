56857d5838195db4a50b0a215f7c735b4a976380
Tien Do <tiendq@gmail.com>
2022-02-16 09:56:03 +0700

    add events

--- contracts/MyFaucet.sol
+++ contracts/MyFaucet.sol
@@ -1,14 +1,44 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.9;
 
 contract MyFaucet {
+  address private immutable owner;
+  address private _lastDeposit;
+  address private _lastWithdrawal;
+
+  event Withdrawal(address indexed _to, uint _amount);
+	event Deposit(address indexed _from, uint _amount);
+
   constructor() {
+    owner = msg.sender;
   }
 
-  receive () external payable {}
+  modifier isOwner {
+    require(msg.sender == owner, 'Only the contract owner can call this function');
+    _;
+  }
+
+  // Accept any incoming amount.
+  receive() external payable {
+    _lastDeposit = msg.sender;
+    emit Deposit(_lastDeposit, msg.value);
+  }
 
   function withdraw(uint _amount) public {
-    require(_amount <= 1000000000000000000);
-    payable(msg.sender).transfer(_amount);
+    // require(_amount <= 1000000000000000000);
+    require(_amount <= 1 ether);
+    require(address(this).balance >= _amount, 'Insufficient balance in faucet for withdrawal request');
+
+    _lastWithdrawal = msg.sender;
+    payable(_lastWithdrawal).transfer(_amount);
+    emit Withdrawal(_lastWithdrawal, _amount);
+  }
+
+  function lastDeposit() public view isOwner returns (address) {
+    return _lastDeposit;
+  }
+
+  function lastWithdrawal() public view isOwner returns (address) {
+    return _lastWithdrawal;
   }
 }
--- docs/cli.md
+++ docs/cli.md
@@ -1,11 +1,13 @@
 ```bash
-eth address:balance 0xb9E27a5bBA4Dfe974adFAdE79B99f84715cfbA70 --network ganache
+eth address:balance 0xC169fb1dD592ad02190Ff99dd595BFa57a2517A3 --network ganache
 
-eth transaction:send --pk key --to 0xe51ea3D582E61B4214F050F4D6f00d19AC0Ef96A --value 10000000000000000000 --network ganache
+eth transaction:send --pk 9627b3dd789f8fce55c03f6781fdb5a202d584be7f7511e50f4183e42b56b728 --to 0xC169fb1dD592ad02190Ff99dd595BFa57a2517A3 --value 10000000000000000000 --network ganache
 
-eth abi:add myFaucet ./build/contracts/MyFaucet.json
+eth abi:update myFaucet ./build/contracts/MyFaucet.json
 
-eth contract:send --network ganache --pk key myFaucet@0xe51ea3D582E61B4214F050F4D6f00d19AC0Ef96A 'withdraw("1000000000000000000")'
+eth contract:send --network ganache --pk 4a2ef800121e51fe4780982e9e9796f785f907465b11c33dc1f81a2d127b586b myFaucet@0xC169fb1dD592ad02190Ff99dd595BFa57a2517A3 'withdraw("1000000000000000000")'
+
+eth contract:call --network ganache myFaucet@0xC169fb1dD592ad02190Ff99dd595BFa57a2517A3 'lastWithdrawal()'
 
 echo $((0x430e23400))
 ```
